a977d11a78a3eecdf2acf888bbbc5b11332bf936
Sait Özdemir <sazdemir1@gmail.com>
2022-06-16 23:40:08 +0300

    edit

--- WebApi/Middleware/CustomExeptionMiddleware.cs
+++ WebApi/Middleware/CustomExeptionMiddleware.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Diagnostics;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace WebApi.Middleware
 {
@@ -15,10 +18,32 @@ namespace WebApi.Middleware
         }
         public async Task Invoke(HttpContext context)
         {
-            string message = "[Request] HTTP " + context.Request.Method + "-" + context.Request.Path;
-            Console.WriteLine(message);
-            await _next(context);
-        }       
+            var watch = Stopwatch.StartNew();
+            try
+            { 
+                string message = "[Request] HTTP " + context.Request.Method + "-" + context.Request.Path;
+                Console.WriteLine(message);
+                await _next(context);
+                watch.Stop();
+                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " +watch.Elapsed.TotalMilliseconds+ " ms";
+                System.Console.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                watch.Stop();
+                await  HandleException(context, ex, watch);
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+        {
+            string message = "[Error] HTTP " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message " + ex.Message + " in " +watch.Elapsed.TotalMilliseconds+ " ms";
+            System.Console.WriteLine(message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var result = JsonConvert.SerializeObject(new {error = ex.Message}, Formatting.None);
+            return context.Response.WriteAsync(result);
+        }
     }
     public static class CustomExeptionMiddlewareExtension
     {
